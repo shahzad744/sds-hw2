e4aabbc7943c6477d7c4be2dfdb76787641d77dd
Shahzad Amin <shahzad.744@gmail.com>
2018-12-03 18:03:13 +0100

    final checkin

--- Main.R
+++ Main.R
@@ -3,16 +3,16 @@ stock <- read.csv("stocks.csv")
 symbols <- stock[["Symbol"]]
 data <- NULL
 for (row in symbols) {
-    if (is.null(data)) {
-        data <- suppressWarnings(get.hist.quote(instrument = row, start = "2003-01-01", end = "2008-01-01", quote = "Close", provider = "yahoo", drop = TRUE))
-
-    }
-    else {
-        temp <- suppressWarnings(get.hist.quote(instrument = row, start = "2003-01-01", end = "2008-01-01", quote = "Close", provider = "yahoo", drop = TRUE))
-        data <- cbind(data, temp)
-
-    }
-
+  if (is.null(data)) {
+    data <- suppressWarnings(get.hist.quote(instrument = row, start = "2003-01-01", end = "2008-01-01", quote = "Close", provider = "yahoo", drop = TRUE))
+    
+  }
+  else {
+    temp <- suppressWarnings(get.hist.quote(instrument = row, start = "2003-01-01", end = "2008-01-01", quote = "Close", provider = "yahoo", drop = TRUE))
+    data <- cbind(data, temp)
+    
+  }
+  
 }
 cMatrix <- as.matrix(data)
 colnames(cMatrix) <- symbols
